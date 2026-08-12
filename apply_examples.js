d432e80e509d3a4eacb5e65633704756e3d47829
mustafasacar35 <mustafasacar@hotmail.com>
2026-08-12 19:40:25 +0300

    feat: Add example sentences to all 79 nouns in Ders 1-2 and Ders 3

---
+++ apply_examples.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const examples = JSON.parse(fs.readFileSync('noun_examples.json', 'utf8'));
+
+function applyExamplesToFile(filename) {
+  const vocab = JSON.parse(fs.readFileSync(filename, 'utf8'));
+  let updatedCount = 0;
+  
+  vocab.words = vocab.words.map(w => {
+    if (w.type === 'isim' && examples[w.bg.toLowerCase()]) {
+      w.examples = examples[w.bg.toLowerCase()];
+      updatedCount++;
+    }
+    return w;
+  });
+  
+  if (updatedCount > 0) {
+    fs.writeFileSync(filename, JSON.stringify(vocab, null, 2), 'utf8');
+    console.log(`Applied examples to ${updatedCount} nouns in ${filename}`);
+  }
+}
+
+applyExamplesToFile('src/data/vocabulary/vocab_ders_1_2.json');
+applyExamplesToFile('src/data/vocabulary/vocab_ders_3.json');
